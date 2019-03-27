befe4aa9ad54c9f4ecc1f3c2779b3d44a790e7d2
Chris Lewis <13chlewis@gmail.com>
2019-03-27 09:25:57 -0600

    Began writing test for station search, currently testing api calls with Postman

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '5.1.4'
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.6'
 gem 'sass-rails', '~> 5.0.6'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'uglifier', '>= 1.3.0'
@@ -11,20 +11,21 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'faraday'
 gem 'figaro'
+gem 'factory_bot_rails'
 
 group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
+  gem 'database_cleaner'
+  gem 'pry'
+  gem 'launchy'
+  gem 'webmock'
+  gem 'vcr'
+  gem 'capybara'
+  gem 'shoulda-matchers'
 end
 
 group :development do
   gem 'web-console', '~> 3.5.1'
   gem 'listen'
 end
-
-group :test do
-  gem 'vcr'
-  gem 'webmock'
-  gem 'capybara'
-end
-
--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,22 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+
+  scenario 'and search for stations' do
+    # As a user
+    # When I visit "/"
+    visit '/'
+    fill_in 'q', with: 80206
+    # And I fill in the search form with 80206 (Note: Use the existing search form)
+    # And I click "Locate"
+    click_button 'Locate'
+    # Then I should be on page "/search"
+    expect(current_path).to eq('/search')
+    # Then I should see the total results of the stations that match my query.
+    expect(page).to have_content()
+    # Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+    # And the stations should be limited to Electric and Propane
+    # And the stations should only be public, and not private, planned or temporarily unavailable.
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
 end
